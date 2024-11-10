ddab300353a5340fba3058299d85b78cf55e10b6
Tan <lenhattan2313@gmail.com>
2024-11-10 22:02:32 +0700

    Fixed interval leetcode

--- DSA/disjointIntervals.py
+++ DSA/disjointIntervals.py
@@ -5,7 +5,7 @@ class Solution:
         prev_s, prev_e = arr[0]
 
         for s, e in arr:
-            if s <= prev_s:
+            if s <= prev_e:
                 pass
             else:
                 prev_s, prev_e = s, e
@@ -13,7 +13,18 @@ class Solution:
 
         return count
 
+    def eraseOverlapIntervals(self, arr) -> int:
+        count = 0
+        prev_e = arr[0][1]
+
+        for i in range(1, len(arr)):
+            if arr[i][0] >= prev_e:
+                prev_e = arr[i][1]
+            else:
+                count += 1
+        return count
 #Time complexity: O(n log(n))
 #Space complexity: O(1)
 solution = Solution()
-print(solution.disjointInvervals([[1,2], [2, 10],[4,6]]))
+print(solution.disjointInvervals([[1,2], [2, 10],[4,6], [3, 4]]))
+[1,2], [3, 4], [4, 6], [2, 10]
